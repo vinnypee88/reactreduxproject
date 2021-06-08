6dc5d4ef818664243fa4ba9788641989172db67d
Vinny <v_p88@hotmail.com>
2021-06-08 15:06:38 +0100

    initial attempt to fetch data from Reddit API

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ import Post from './components/Post/Post'
 
 
 function App() {
+
+ 
+async function getData () {
+  const response = await fetch('https://www.reddit.com/r/popular.json')
+  const json = await response.json()
+  const data = json.data.children
+  return data.map (item =>{
+    return item.data
+  })
+}
+
+
+const state = {
+  state: getData()
+}
+console.log(state.state)
+
+
   return (
     <div className="App"> 
     
@@ -16,6 +34,7 @@ function App() {
       <Post />
       <Post />
       <Post />
+      
       </main>
       <aside>
         <p>aside</p>
